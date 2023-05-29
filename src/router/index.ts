5afb9ad477804985d78daea8d01087442ca147d1
slince-zero <1711257704@qq.com>
2023-05-29 20:00:36 +0800

    11

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ const routes: Array<RouteRecordRaw> = [
     path: '/login',
     name: 'login',
     component: () => import('../views/LoginView.vue'),
-  },
+  }
 ]
 
 const router = createRouter({
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,38 +1,37 @@
 <template>
-  <div>
     <el-form :model="form" :rules="rules" label-width="120px">
-      <el-form-item label="用户名" prop="form.name">
-        <el-input v-model="form.name" />
-      </el-form-item>
-      <el-form-item label="密码" prop="form.password">
-        <el-input v-model="form.password" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-      </el-form-item>
+        <el-form-item label="用户名" prop="name">
+            <el-input v-model="form.name" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+            <el-input v-model="form.password" />
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="onSubmit">Create</el-button>
+        </el-form-item>
     </el-form>
-  </div>
 </template>
-
+  
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
 const form = ref({
-  name: '',
-  password: '',
+    name: '',
+    password: '',
 })
 
+
 const rules = reactive({
-  name: [
-    { required: true, message: 'Please input Activity name', trigger: 'blur' },
-  ],
-  password: [
-    { required: true, message: 'Please input Activity name', trigger: 'blur' },
-  ],
+    name: [
+        { required: true, message: 'Please input Activity name', trigger: 'blur' },
+    ],
+    password: [
+        { required: true, message: 'Please input Activity name', trigger: 'blur' },
+    ],
 })
 
 const onSubmit = () => {
-  console.log('submit')
+
 }
 </script>
-
+  
 <style></style>
